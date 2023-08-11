e75d9f6849fc1178e259c61367fc4aa2efdc2636
joyyg0606 <joyyg0606@gmail.com>
2023-08-11 15:58:05 -0400

    Arrays and vectors

---
+++ Arrays & Vectors/arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    char vowels[]{'a', 'e', 'i', 'o', 'u'};
+    cout << "\nThe first vowel is: " << vowels[0] << endl;
+    cout << "The last vowel is: " << vowels[4] << endl;
+
+    return 0;
+}
