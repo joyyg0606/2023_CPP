c2f163eeb4d3665b0ff42ad298e0abea6c6a68a8
joyyg0606 <joyyg0606@gmail.com>
2023-08-12 19:09:08 -0400

    Statements & Operators

--- Statements & Operators/main.cpp
+++ Statements & Operators/main.cpp
@@ -4,24 +4,27 @@ using namespace std;
 
 int main() {
 
-    // int num1{10};
-    // int num2{20};
-
-    // num1 = num2 = 1000;
-    // assigns right to left
-
-    // cout << "num 1 is " << num1 << endl;
-    // cout << "num 2 is " << num2 << endl;
-
-    // cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
-
-    // cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
-
-    // cout << num2 << " / " << num1 << " = " << num2 / num1 << endl;
-
-    // cout << num1 << " % " << num2 << " = " << num1 % num2 << endl;
-
     /*===================================================================================
+
+    int num1{10};
+    int num2{20};
+
+    num1 = num2 = 1000;
+    assigns right to left
+
+    cout << "num 1 is " << num1 << endl;
+    cout << "num 2 is " << num2 << endl;
+
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+
+    cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+
+    cout << num2 << " / " << num1 << " = " << num2 / num1 << endl;
+
+    cout << num1 << " % " << num2 << " = " << num1 % num2 << endl;
+
+    =====================================================================================
+
     const double usd_per_eur{1.9};
 
     cout << "Welcome to the EUR to USD converter\n " << endl;
@@ -35,27 +38,28 @@ int main() {
 
     cout << euros << " euro(s) is equivalent to " << dollars << " dollar(s)" << endl;
     ===================================================================================*/
+
     int counter{10};
     int result{0};
 
-    // Example 1 - simple increment
-    // cout << "Counter: " << counter << endl;
+    /*===================== Example 1 - simple increment ================================
+    cout << "Counter: " << counter << endl;
 
-    // counter = counter + 1;
-    // cout << "Counter: " << counter << endl;
+    counter = counter + 1;
+    cout << "Counter: " << counter << endl;
 
-    // counter++;
-    // cout << "Counter: " << counter << endl;
+    counter++;
+    cout << "Counter: " << counter << endl;
 
-    // ++counter;
-    // cout << "Counter: " << counter << endl;
+    ++counter;
+    cout << "Counter: " << counter << endl;
 
-    // cout << "Counter: " << counter << endl;
+    cout << "Counter: " << counter << endl;
 
-    // result = ++counter;
-    // cout << "Counter: " << counter << endl;
-    // cout << "Result: " << result << endl;
-
+    result = ++counter;
+    cout << "Counter: " << counter << endl;
+    cout << "Result: " << result << endl;
+    ===================================================================================*/
     cout << "Counter: " << counter << endl;
 
     result = ++counter + 10;
@@ -63,7 +67,6 @@ int main() {
 
     cout << "Counter: " << counter << endl;
     cout << "Result: " << result << endl;
-
 
     cout << endl;
     return 0;
