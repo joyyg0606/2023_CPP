71b47dd2f0dc6f29e0e3548057e9554283981bf4
joyyg0606 <joyyg0606@gmail.com>
2023-08-25 17:33:06 -0400

    classes and objects

---
+++ Classes and Objects/move_constructors.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Move {
+private:
+    int *data;
+public:
+    void set_data_value(int d) { *data = d; }
+    int get_data_value() { return *data; }
+    //Constructor
+    Move(int d);
+    //copy constructor
+    Move(const Move &source);
+    //Move constructor
+    // Move(Move &&source) noexcept;
+    //destructor
+    ~Move();
+};
+
+Move::Move(int d) {
+    data = new int;
+    *data = d;
+    cout << "Constructor for: " << d << endl;
+}
+
+//Copy ctor
+Move::Move(const Move &source) : Move {*source.data} {
+    cout << "Copy constructor - deep copy for: " << *data << endl;
+}
+
+// //Move ctor
+// Move::Move(Move &&source) noexcept : data{source.data} {
+//     source.data = nullptr;
+//     cout << "Move constructor - moving resource: " << *data << endl;
+// }
+
+Move::~Move () {
+    if (data != nullptr) {
+        cout << "Destructor freeing data for : " << *data << endl;
+    } else {
+        cout << "Destructor freeing data for nullptr" << endl;
+    }
+    delete data;
+}
+
+int main()
+{
+    vector<Move> vec;
+
+    vec.push_back(Move{10});
+
+    // vec.push_back(Move{20});
+    // vec.push_back(Move{30});
+    // vec.push_back(Move{40});
+    // vec.push_back(Move{50});
+    // vec.push_back(Move{60});
+    // vec.push_back(Move{70});
+    // vec.push_back(Move{80});
+
+    cout << endl;
+    return 0;
+}
