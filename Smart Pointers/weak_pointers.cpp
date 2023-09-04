b5aa738ec1a24d0cbaa084a5abb730ea3eb03ec9
joyyg0606 <joyyg0606@gmail.com>
2023-09-04 17:31:53 -0400

    weak pointers

---
+++ Smart Pointers/weak_pointers.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <memory>
+
+
+using namespace std;
+
+class B;
+
+class A {
+    std::shared_ptr<B> b_ptr;
+public:
+    void set_B(std::shared_ptr<B> &b) {
+        b_ptr = b;
+    }
+    A() { cout << "A Constructor" << endl; }
+    ~A() { cout << "A Destructor" << endl; }
+};
+
+class B {
+    std::shared_ptr<A> a_ptr;
+public:
+    void set_A(std::shared_ptr<A> &a) {
+        a_ptr = a;
+    }
+    B() { cout << "B Constructor" << endl; }
+    ~B() { cout << "B Destrcutor" << endl; }
+};
+
+int main() {
+    shared_ptr<A> a = make_shared<A>();
+    shared_ptr<B> b = make_shared<B>();
+    a->set_B(b);
+    b->set_A(a);
+
+    return -;
+}
